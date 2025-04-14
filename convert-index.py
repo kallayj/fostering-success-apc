8a2a02fb67a39bdb6757fa23cb3c9edfa879bf25
Jonathan Kallay <118477617+kallayj@users.noreply.github.com>
2025-04-14 18:50:02 +0000

    xlsx to md tables

--- convert-index.py
+++ convert-index.py
@@ -2,7 +2,7 @@ import openpyxl
 from openpyxl import load_workbook
 
 # Load the Excel file
-wb = load_workbook('conservation.xlsx', data_only=False)
+wb = load_workbook('rotation_shm.xlsx', data_only=False)
 sheet = wb.active
 
 # Create markdown table
@@ -18,5 +18,5 @@ for row in list(sheet.iter_rows())[1:]:
             row_text.append(str(cell.value) if cell.value is not None else "")
     markdown += "| " + " | ".join(row_text) + " |\n"
 
-with open('output.md', 'w') as f:
+with open('rotation_shm.md', 'w') as f:
     f.write(markdown)
